d00bea7e72ee3cac7ad1a2d1780c99b8645892fc
adilifescience-hub <adi.lifescience@gmail.com>
2026-04-05 08:37:31 +0530

    Readit.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,192 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(ReaditApp());
+}
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class ReaditApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter_native',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      //home: MyHomePage(title: 'Flutter Demo Home Page'),
-      home: RaisedButtonExample(),
+      title: 'Readit',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginScreen(),
     );
   }
 }
 
+class LoginScreen extends StatelessWidget {
+  final TextEditingController nameController = TextEditingController();
 
-class RaisedButtonExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Readit Login")),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: "Enter Name"),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              child: Text("Login"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        HomeScreen(username: nameController.text),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
 
+class HomeScreen extends StatefulWidget {
+  final String username;
+  HomeScreen({required this.username});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int points = 0;
+
+  final List<String> books = [
+    "Focus Power",
+    "Success Habits",
+    "Mind Growth"
+  ];
+
+  void addPoints(int p) {
+    setState(() {
+      points += p;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter_Native'),
+        title: Text("Welcome ${widget.username}"),
+        actions: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Center(child: Text("Points: $points")),
+          )
+        ],
       ),
-      body: Center(
-        //Fairly easy, just create a RaisedButton, which has been provided by flutter already. Use style options to customize other properties.
-        child: RaisedButton(
-          color: Colors.blue,
-          child: Text('Press Me'),
-          onPressed: (){
-              print('button clicked');
-          }
-        ),
+      body: ListView.builder(
+        itemCount: books.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(books[index]),
+            onTap: () async {
+              int earned = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ReadingScreen(book: books[index]),
+                ),
+              );
+              if (earned != null) addPoints(earned);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ReadingScreen extends StatefulWidget {
+  final String book;
+  ReadingScreen({required this.book});
+
+  @override
+  _ReadingScreenState createState() => _ReadingScreenState();
+}
+
+class _ReadingScreenState extends State<ReadingScreen> {
+  int seconds = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  void startTimer() {
+    Future.doWhile(() async {
+      await Future.delayed(Duration(seconds: 1));
+      setState(() => seconds++);
+      return true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.book)),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Text("Reading content here...\n\nTime: $seconds sec"),
+          ),
+          ElevatedButton(
+            child: Text("Finish & Take Quiz"),
+            onPressed: () async {
+              int result = await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => QuizScreen()),
+              );
+              Navigator.pop(context, result);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class QuizScreen extends StatefulWidget {
+  @override
+  _QuizScreenState createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  int score = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Quiz")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Did you read properly?"),
+          ElevatedButton(
+            child: Text("Yes"),
+            onPressed: () {
+              score = 10;
+              Navigator.pop(context, score);
+            },
+          ),
+          ElevatedButton(
+            child: Text("No"),
+            onPressed: () {
+              Navigator.pop(context, 0);
+            },
+          )
+        ],
       ),
     );
   }
